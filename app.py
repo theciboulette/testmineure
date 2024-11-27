989b1943271d5126414f7048ba5feee9e0c59271
theciboulette <guillaume_valentin@hotmail.fr>
2024-11-27 11:34:49 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,13 +6,17 @@ values, target = load_wine(return_X_y=True)
 target_names = load_wine().target_names
 feature_names = load_wine().feature_names
 
-st.title("Bonjour l'alcoolique !'")
+st.title("Bonjour l'alcoolique !")
 
 df = pd.DataFrame(values, columns=feature_names)
 df
 plot=df["alcohol"]
-st.write("regarde ce char")
-st.line_chart(plot)
+
+bot = st.checkbox("Regarde ce char")
+
+if bot:
+    st.write("cool")
+    st.line_chart(plot)
 
 
 
